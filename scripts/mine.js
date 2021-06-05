06bb708d8063f4f6fa154e127171fb3b102bd272
sharo-jef <cleared.for.approach.runway23@gmail.com>
2021-06-06 03:45:11 +0900

    deleted script file

--- scripts/mine.js
+++
@@ -1,6 +0,0 @@
-const range = 3;
-for (let i = Math.ceil(range / 2 - range); i < range / 2; i++) {
-    for (let j = Math.ceil(range / 2 - range); j < range / 2; j++) {
-        console.log(`execute as @e[tag=worker] at @s positioned ~${i} ~-1 ~${j} unless block ~ ~ ~ minecraft:bedrock run function auto-excavator:service/mine/mine`);
-    }
-}
